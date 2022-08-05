b1c53f9d7d4e83d58435471a05ba8118a797c780
berdanakyurek <berdanakyurek17@gmail.com>
2022-08-05 12:03:50 +0300

    completed routing function

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+      <nav>
+          <a href="/">Home</a>
+          <span>&nbsp;&nbsp;</span>
+          <a href="/about">About</a>
+          <span>&nbsp;&nbsp;</span>
+          <a href="/contact">Contact</a>
+      </nav>
+  );
+}
+
+export default Nav;
