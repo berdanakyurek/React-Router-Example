2e9b3decc2a242caf88f6f9fdeecc541ffe1aff6
berdanakyurek <berdanakyurek17@gmail.com>
2022-08-19 08:58:50 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import Nav from "./Nav.js";
+import Home from "./Home.js";
+import Contact from "./Contact.js";
+import About from "./About.js";
+
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
+import React from "react";
+
+
 function App() {
-  return (
-      null
-  );
+    return (
+        <div>
+            <Router>
+                <Nav/>
+                <Routes>
+                    <Route path="/" exact element={<Home/>}/>
+                    <Route path="/about" element={<About/>} exact />
+                    <Route path="/contact" element={<Contact/>} exact />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
